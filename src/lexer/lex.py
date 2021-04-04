defca2985c15f2eaaa50cea160271d0af0963dec
sebastianmoline <sebastianmoline@gmail.com>
2021-04-04 17:47:58 -0300

    Update Lexer

--- src/lexer/lex.py
+++ src/lexer/lex.py
@@ -1,103 +1,99 @@
 #List of token names
 tokens = (
 
-    #symbols
-    'INT',
-    'STRING',
-    'ID',
+	#symbols
+	'INT',
+	'STRING',
+	'ID',
 
-    #ignored symbols the lexer or parser shouldn't mind
-    #'NEW_LINE',
-    #'TAB',
-    #'SPACE',
-    #'COMMENT',
+	#ignored symbols the lexer or parser shouldn't mind
+	#'NEW_LINE',
+	#'TAB',
+	#'SPACE',
+	'COMMENT',
 
-    #punctuation symbols
-    'COMMA',
-    'COLON',
-    'SEMICOLON',
-    'LPAREN',
-    'RPAREN',
-    'LBRACK',
-    'RBRACK',
-    'LBRACE',
-    'RBRACE',
-    'DOT',
-    'PLUS',
-    'MINUS',
-    'TIMES',
-    'DIVIDE',
-    'EQ',
-    'NEQ',
-    'LT',
-    'LE',
-    'GT',
-    'GE',
-    'AND',
-    'OR',
-    'ASSIGN',
+	#punctuation symbols
+	'COMMA',
+	'COLON',
+	'SEMICOLON',
+	'LPAREN',
+	'RPAREN',
+	'LBRACK',
+	'RBRACK',
+	'LBRACE',
+	'RBRACE',
+	'DOT',
+	'PLUS',
+	'MINUS',
+	'TIMES',
+	'DIVIDE',
+	'EQ',
+	'NEQ',
+	'LT',
+	'LE',
+	'GT',
+	'GE',
+	'AND',
+	'OR',
+	'ASSIGN',
 
-    #keywords
-    'ARRAY',
-    'IF',
-    'THEN',
-    'ELSE',
-    'WHILE',
-    'FOR',
-    'TO',
-    'DO',
-    'LET',
-    'IN',
-    'END',
-    'OF',
-    'BREAK',
-    'NIL',
-    'FUNCTION',
-    'VAR',
-    'TYPE',
+	#keywords
+	'ARRAY',
+	'IF',
+	'THEN',
+	'ELSE',
+	'WHILE',
+	'FOR',
+	'TO',
+	'DO',
+	'LET',
+	'IN',
+	'END',
+	'OF',
+	'BREAK',
+	'NIL',
+	'FUNCTION',
+	'VAR',
+	'TYPE',
 )
 
 reservedKeywords = (
-    'array',
-    'if',
-    'then',
-    'else',
-    'while',
-    'for',
-    'to',
-    'do',
-    'let',
-    'in',
-    'end',
-    'of',
-    'break',
-    'nil',
-    'function',
-    'var',
-    'type',
+	'array',
+	'if',
+	'then',
+	'else',
+	'while',
+	'for',
+	'to',
+	'do',
+	'let',
+	'in',
+	'end',
+	'of',
+	'break',
+	'nil',
+	'function',
+	'var',
+	'type',
 )
 
-# Regular expression rules with some actions required.
+# Regular expression rules with some actions required
 # Reads an integer value
 def t_INT(t):
-    r'\d+'
-    t.value = int(t.value)
-    return t
+	r'\d+'
+	t.value = int(t.value)
+	return t
 
-# Reads a string value having the following structure:
-#   Starts with the character "
-#   Combination of 0 or more printable characters, spaces or escape sequences
-#   Finishes with the character "
+# reads
 t_STRING = r'\"([^\\\"]|(\\n)|(\\t)|(\\\^c)|(\\[0-9][0-9][0-9])|(\\\")|(\\\\)|(\\[\s\t\n\f]+\\))*\"'
 
-# Reads an id value. If the id is a reserved keyword, it shows it instead
 def t_ID(t):
-    r'[a-zA-Z][a-zA-Z_0-9]*'
-    if t.value in reservedKeywords:
-        t.type = t.value.upper()
-    return t
+	r'[a-zA-Z][a-zA-Z_0-9]*'
+	if t.value in reservedKeywords:
+		t.type = t.value.upper()
+	return t
 
-# Regular expression rules for simple tokens.
+# Regular expression rules for simple tokens
 t_COMMA = r','
 t_COLON = r':'
 t_SEMICOLON = r';'
@@ -124,46 +120,45 @@ t_ASSIGN = r'\:\='
 
 # Define a rule so we can track line numbers
 def T_newline(t):
-    r'\n+'
-    t.lexer.lineno += len(t.value)
+	r'\n+'
+	t.lexer.lineno += len(t.value)
 
 # A string containing ignored characters (spaces and tabs)
 t_ignore = ' \t'
 
-# Esto funciona correctamente? Chequear si es necesario dejar el token activo arriba
+# Esto funciona correctamente?
 t_ignore_COMMENT = r'\/\*.*\*\/'
 
 # Error handling rule
 def t_error(t):
-    print("Illegal character '%s' " %t.value[0])
-    t.lexer.skip(1)
+	print("Illegal character '%s' " %t.value[0])
+	t.lexer.skip(1)
 
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
 	# Build the lexer
-    # hace falta bajar ply para esto
-	import ply.lex as lex
-	import sys
+from .ply import lex as lex
+import sys
 
-	lexer = lex.lex()
+lexer = lex.lex()
 
-	if len(sys.argv) > 1:
-		f = open(sys.argv[1],"r")
-		data = f.read()
-		f.close()
-	else:
-		data = ""
-		while True:
-			try:
-				data += raw_input() + "\n"
-			except:
-				break
-
-	lex.input(data)
-
-	# Tokenize
+if len(sys.argv) > 1:
+	f = open(sys.argv[1],"r")
+	data = f.read()
+	f.close()
+else:
+	data = ""
 	while True:
-	    tok = lex.token()
-	    if not tok:
-            break      # No more input
-	    print tok
+		try:
+			data += raw_input() + "\n"
+		except:
+			break
+
+lex.input(data)
+
+# Tokenize
+while True:
+	tok = lex.token()
+	if not tok:
+		break      # No more input
+	print(tok)
