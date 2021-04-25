67e97d47efcbe70a055b6ac4e5f4c850760dec6f
Weheineman <gi4nn1@yahoo.de>
2021-04-25 15:14:01 -0300

    Finished creating node types.

--- src/parser/ast_nodes.py
+++ src/parser/ast_nodes.py
@@ -2,39 +2,48 @@ from abc import ABC, abstractmethod
 from enum import Enum
 from dataclasses import dataclass
 
-
-# TODO: Use @ dataclass
-# TODO: Try to use python lists. [] es feliz. null pointer no es feliz.
-
-
 @dataclass
 class Position:
     lineNumber: int
 
-
-class ASTNode:
-    pass
-
-
-class Type(ASTNode):
-    pass
-
-
+# TODO: Replace dummy ASTNode definiton.
 # class ASTNode(ABC):
 # @abstractmethod
 # def pretty_print(self):
 #     pass
 
-
-@dataclass
-class Field(ASTNode):
-    name: str
-    type: str
-
+class ASTNode:
+    pass
 
 class Declaration(ASTNode):
     pass
 
+class Type(ASTNode):
+    pass
+
+class Expression(ASTNode):
+    pass
+
+class Variable(ASTNode):
+    pass
+
+class Oper(Enum):
+    plus = 1
+    minus = 2
+    times = 3
+    divide = 4
+    eq = 5
+    neq = 6
+    lt = 7
+    le = 8
+    gt = 9
+    ge = 10
+
+# DECLARATION
+
+@dataclass
+class DeclarationBlock(ASTNode):
+    declarationList: [Declaration]
 
 @dataclass
 class TypeDec(ASTNode):
@@ -44,12 +53,18 @@ class TypeDec(ASTNode):
 
 @dataclass
 class TypeDecBlock(Declaration):
-    type: [TypeDec]
+    typeDecList: [TypeDec]
 
 
 @dataclass
 class NameTy(Type):
     name: str
+
+
+@dataclass
+class Field(ASTNode):
+    name: str
+    type: str
 
 
 @dataclass
@@ -62,258 +77,128 @@ class ArrayTy(Type):
     array: str
 
 
-# class Variable(ASTNode):
-#     pass
+@dataclass
+class VariableDec(Declaration):
+    name: str
+    type: Type # TODO: Make this optional.
+    exp: Expression
 
+@dataclass
+class FunctionDec(ASTNode):
+    name: str
+    params: [Field]
+    returnType: Type # TODO: Make this optional.
+    body: Expression
 
-# class SimpleVar(Variable):
-#     def __init__(self, position=-1, symbol):
-#         self.position = position
-#         self.symbol = symbol
+@dataclass
+class FunctionDecBlock(Declaration):
+    fuctionDecList: [FunctionDec]
 
+# EXPRESSION
 
-# class FieldVar(Variable):
-#     def __init__(self, position=-1, variable, symbol):
-#         self.position = position
-#         self.variable = variable
-#         self.symbol = symbol
+@dataclass
+class VarExp(Expression):
+    var: Variable
 
+@dataclass
+class NilExp(Expression):
+    position: Position
 
-# class SubscriptVar(Variable):
-#     def __init__(self, position=-1, variable, expression):
-#         self.position = position
-#         self.variable = variable
-#         self.expression = expression
+@dataclass
+class IntExp(Expression):
+    int: int
 
+@dataclass
+class StringExp(Expression):
+    string: str
 
-# class Expression(ASTNode):
-#     pass
+@dataclass
+class CallExp(Expression):
+    func: str
+    args: [Expression]
 
+@dataclass
+class OpExp(Expression):
+    oper: Oper
+    left: Expression
+    right: Expression
 
-# class VarExp(Expression):
-#     def __init__(self, position=-1, variable):
-#         self.position = position
-#         self.variable = variable
+@dataclass
+class ExpField(ASTNode):
+    name: str
+    exp: Expression
 
+@dataclass
+class RecordExp(Expression):
+    type: str
+    fields: [ExpField]
 
-# class NilExp(Expression):
-#     def __init__(self, position):
-#         self.position = position
 
+@dataclass
+class SeqExp(Expression):
+    seq: [Expression]
 
-# class IntExp(Expression):
-#     def __init__(self, position=-1, integer):
-#         self.position = position
-#         self.integer = integer
 
+@dataclass
+class AssignExp(Expression):
+    var: Variable
+    exp: Expression
 
-# class StringExp(Expression):
-#     def __init__(self, position=-1, string):
-#         self.position = position
-#         self.string = string
 
+@dataclass
+class IfExp(Expression):
+    test: Expression
+    then: Expression
+    elsee: Expression #TODO: Por YHVH encontrar un nombre mejor.
 
-# class CallExp(Expression):
-#     def __init__(self, position=-1, function, arguments):
-#         self.position = position
-#         self.function = function
-#         self.arguments = arguments
 
+@dataclass
+class WhileExp(Expression):
+    test: Expression
+    body: Expression
 
-# class OpExp(Expression):
-#     def __init__(self, position=-1, operator, left, right):
-#         self.position = position
-#         self.operator = operator
-#         self.left = left
-#         self.right = right
 
+@dataclass
+class BreakExp(Expression):
+    position: Position
 
-# class RecordExp(Expression):
-#     def __init__(self, position=-1, _type, fields):
-#         self.position = position
-#         self.type = _type
-#         self.fields = fields
 
 
-# class SeqExp(Expression):
-#     def __init__(self, position=-1, sequence):
-#         self.position = position
+@dataclass
+class ForExp(Expression):
+    var: Variable
+    lo: Expression
+    hi: Expression
+    body: Expression
 
 
-# class AssignExp(Expression):
-#     def __init__(self, position=-1, variable, expression):
-#         self.position = position
-#         self.variable = variable
-#         self.expression = expression
 
+@dataclass
+class LetExp(Expression):
+    decs: DeclarationBlock
+    body: [Expression]
 
-# class IfExp(Expression):
-#     def __init__(self, position=-1, test, then, elsee):
-#         self.position = position
-#         self.test = test
-#         self.then = then
-#         self.elsee = elsee  # TODO: Por Dios encontremos un nombre mejor.
 
 
-# class WhileExp(Expression):
-#     def __init__(self, position=-1, test, body):
-#         self.position = position
-#         self.test = test
-#         self.body = body
+@dataclass
+class ArrayExp(Expression):
+    type: str
+    size: Expression
+    init: Expression
 
+# VARIABLE
 
-# class BreakExp(Expression):
-#     def __init__(self, position):
-#         self.position = position
+@dataclass
+class SimpleVar(Variable):
+    sym: str
 
 
-# class ForExp(Expression):
-#     def __init__(self, position=-1, variable, low, high, body):
-#         self.position = position
-#         self.variable = variable
-#         self.low = low
-#         self.high = high
-#         self.body = body
+@dataclass
+class FieldVar(Variable):
+    var: Variable
+    sym: str
 
-
-# class LetExp(Expression):
-#     def __init__(self, position=-1, declarations, body):
-#         self.position = position
-#         self.declarations = declarations
-#         self.body = body
-
-
-# class ArrayExp(Expression):
-#     def __init__(self, position=-1, _type, size, init):
-#         self.position = position
-#         self.type = _type
-#         self.size = size
-#         self.init = init
-
-
-# class Declaration(ASTNode):
-#     pass
-
-
-# class FunctionDec(Declaration):
-#     def __init__(self, position=-1, function):
-#         self.position = position
-#         self.function = function
-
-
-# class VarDec(Declaration):
-#     def __init__(self, position=-1, symbol, _type, init):
-#         self.position = position
-#         self.symbol = symbol
-#         self.type = _type
-#         self.init = init
-
-
-# class TypeDec(Declaration):
-#     def __init__(self, position=-1, _type):
-#         self.position = position
-#         self.type = _type
-
-
-# class Type(ASTNode):
-#     pass
-
-
-# class NameTy(Type):
-#     def __init__(self, position=-1, name):
-#         self.position = position
-#         self.name = name
-
-
-# class RecordTy(Type):
-#     def __init__(self, position=-1, variable, _type, init):
-#         self.position = position
-#         self.variable = variable
-#         self.type = _type
-#         self.init = init
-
-
-# class ArrayTy(Type):
-#     def __init__(self, position=-1, array):
-#         self.position = position
-#         self.array = array
-
-
-# # Que garlopa son estas clases???
-# class Field(ASTNode):
-#     def __init__(self, position=-1, name, _type):
-#         self.position = position
-#         self.name = name
-#         self.type = _type
-
-
-# # TODO: Todas las listas tienen la misma pinta, vale la pena que sean
-# # clases diferentes?
-# class FieldList(ASTNode):
-#     def __init__(self, head, tail):
-#         self.head = head
-#         self.tail = tail
-
-
-# class ExpList(ASTNode):
-#     def __init__(self, head, tail):
-#         self.head = head
-#         self.tail = tail
-
-
-# class Fundec(ASTNode):
-#     def __init__(self, position=-1, name, parameters, result, body):
-#         self.position = position
-#         self.name = name
-#         self.parameters = parameters
-#         self.result = result
-#         self.body = body
-
-
-# class FundecList(ASTNode):
-#     def __init__(self, head, tail):
-#         self.head = head
-#         self.tail = tail
-
-
-# class DecList(ASTNode):
-#     def __init__(self, head, tail):
-#         self.head = head
-#         self.tail = tail
-
-
-# class Namety(ASTNode):
-#     def __init__(self, position=-1, type):
-#         self.position = position
-#         self.type = type
-
-
-# class NametyList(ASTNode):
-#     def __init__(self, head, tail):
-#         self.head = head
-#         self.tail = tail
-
-
-# class Efield(ASTNode):
-#     def __init__(self, position=-1, expression):
-#         self.position = position
-#         self.expression = expression
-
-
-# class EfieldList(ASTNode):
-#     def __init__(self, head, tail):
-#         self.head = head
-#         self.tail = tail
-
-
-# class Oper(Enum):
-#     plus = 1
-#     minus = 2
-#     times = 3
-#     divide = 4
-#     eq = 5
-#     neq = 6
-#     lt = 7
-#     le = 8
-#     gt = 9
-#     ge = 10
+@dataclass
+class SubscriptVar(Variable):
+    var: Variable
+    exp: Expression
