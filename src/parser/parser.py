ffdf6b8867bf899e0cfdcf12aca11e9b8f8bdf75
Weheineman <gi4nn1@yahoo.de>
2021-04-25 15:14:25 -0300

    Finished v1 of the parser. * All the grammar is written. * No positions. * Buggy :(

--- src/parser/parser.py
+++ src/parser/parser.py
@@ -5,94 +5,12 @@ from lexer.lex import tokens
 # TODO: Remove this line after finishing.
 import code
 
-"""
-    decs : decs dec | dec
-
-    dec : tdec | fdec | vdec
-
-    tdec : TYPE ID EQ type
-
-    type : ID | rec_fields | array_def
-
-    rec_fields : LBRACE tfields RBRACE | LBRACE RBRACE
-
-    array_def : ARRAY OF ID
-
-    tfields : type_field | tfields COMMA type_field
-
-    type_field : ID COLON ID
-
-    fdec : FUNCTION ID LPAREN RPAREN EQ exp
-         | FUNCTION ID LPAREN tfields RPAREN EQ exp
-         | FUNCTION ID LPAREN RPAREN COLON ID EQ exp
-         | FUNCTION ID LPAREN tfields RPAREN COLON ID EQ exp
-
-    vdec : VAR ID ASSIGN exp | VAR ID COLON ID ASSIGN exp
-
-    exp : const
-        | seqexp
-        | call
-        | record
-        | opexp
-        | flow
-        | lval
-        | array
-        | assign
-        | let
-
-    const : integer | string | nil
-    integer : INT
-    string : STRING
-    nil : NIL
-
-    seqexp : LPAREN exp_seq RPAREN
-    exp_seq : exp | exp_seq SEMICOLON exp
-
-    call : ID LPAREN RPAREN | ID LPAREN args RPAREN
-    args : args COMMA exp | exp
-
-    record : ID LBRACE RBRACE | ID LBRACE content RBRACE
-    content : ID EQ exp | content COMMA ID EQ exp
-
-    opexp : MINUS exp
-    | exp PLUS exp
-    | exp MINUS exp
-    | exp TIMES exp
-    | exp DIVIDE exp
-    | exp EQ exp
-    | exp NEQ exp
-    | exp LT exp
-    | exp LE exp
-    | exp GT exp
-    | exp GE exp
-    | exp AND exp
-    | exp OR exp
-
-    flow : BREAK
-         | WHILE exp DO exp
-         | IF exp THEN exp
-         | IF exp THEN exp ELSE exp
-         | FOR IS ASSIGN exp TO exp DO exp
-
-    lval : var
-    var : ID | ID opt_var
-    opt_var : DOT ID
-            | DOT ID opt_var
-            | LPAREN exp RPAREN
-            | LPAREN exp RPAREN opt_var
-
-    array : ID LBRACK exp RBRACK OF exp
-
-    assign : var ASSIGN exp
-
-    let : LET decs IN END
-        | LET decs IN exp_seq END
-"""
-
 # flake8: noqa ANN001
 
-start = "type_dec_block"
+start = "expression"
 
+
+# EMPTY
 
 def p_empty(p):
     "empty :"
@@ -103,13 +21,49 @@ def p_empty_list(p):
     "empty_list : empty"
     p[0] = []
 
+# DECLARATION
+
+def p_declaration_block(p):
+    """
+    declaration_block : empty_list
+                      | declaration_list
+    """
+    p[0] = Node.DeclarationBlock(declarationList=p[1])
+
+#TODO: Add variable declaration.
+def p_declaration(p):
+    """
+    declaration : type_dec_block
+                | variable_dec
+                | function_dec_block
+    """
+    p[0] = p[1]
+
+
+# Non-empty declaration list.
+def p_declaration_list(p):
+    """
+    declaration_list : declaration_list_iter
+                     | declaration_list_end
+    """
+    p[0] = p[1]
+
+
+def p_declaration_list_iter(p):
+    "declaration_list_iter : declaration_list declaration"
+    p[0] = p[1]
+    p[0].append(p[2])
+
+
+def p_declaration_list_end(p):
+    "declaration_list_end : declaration"
+    p[0] = [p[1]]
 
 def p_type_dec_block(p):
     """
-    type_dec_block : empty_list
-                   | type_dec_list
+    type_dec_block : type_dec_list
     """
-    p[0] = Node.TypeDecBlock(type=p[1])
+    p[0] = Node.TypeDecBlock(typeDecList=p[1])
 
 
 def p_type_dec(p):
@@ -188,11 +142,326 @@ def p_field_list_end(p):
     "field_list_end : field"
     p[0] = [p[1]]
 
+def p_variable_dec(p):
+    """
+    variable_dec : variable_dec_no_type
+                 | variable_dec_with_type
+    """
+    p[0] = p[1]
 
-# Error rule for syntax errors
+def p_variable_dec_no_type(p):
+    "variable_dec_no_type : VAR ID ASSIGN expression"
+    p[0] = Node.VariableDec(name=p[2], type=None, exp=p[4])
+
+
+def p_variable_dec_with_type(p):
+    "variable_dec_with_type : VAR ID COLON ID ASSIGN expression"
+    p[0] = Node.VariableDec(name=p[2], type=p[4], exp=p[6])
+
+
+def p_function_dec_block(p):
+    """
+    function_dec_block : function_dec_list
+    """
+    p[0] = Node.FunctionDecBlock(functionDecList=p[1])
+
+
+def p_function_dec(p):
+    """
+    function_dec : function_dec_no_type
+                 | function_dec_with_type
+    """
+    p[0] = p[1]
+
+
+def p_function_dec_no_type(p):
+    "function_dec_no_type : FUNCTION ID LPAREN field_list RPAREN EQ expression"
+    p[0] = Node.FunctionDec(name=p[2], params=p[4], returnType=None, body=p[7])
+
+
+
+def p_function_dec_with_type(p):
+    "function_dec_with_type : FUNCTION ID LPAREN field_list RPAREN COLON ID EQ expression"
+    p[0] = Node.FunctionDec(name=p[2], params=p[4], returnType=p[7], body=p[9])
+
+
+# Non-empty function declaration list.
+def p_function_dec_list(p):
+    """
+    function_dec_list : function_dec_list_iter
+                      | function_dec_list_end
+    """
+    p[0] = p[1]
+
+
+def p_function_dec_list_iter(p):
+    "function_dec_list_iter : function_dec_list function_dec"
+    p[0] = p[1]
+    p[0].append(p[2])
+
+
+def p_function_dec_list_end(p):
+    "function_dec_list_end : function_dec"
+    p[0] = [p[1]]
+
+
+# EXPRESSION
+
+def p_expression(p):
+    """
+    expression : var_exp
+               | nil_exp
+               | int_exp
+               | string_exp
+               | call_exp
+               | op_exp
+               | record_exp
+               | seq_exp
+               | assign_exp
+               | if_exp
+               | while_exp
+               | break_exp
+               | for_exp
+               | let_exp
+    """
+    p[0] = p[1]
+
+def p_var_exp(p):
+    "var_exp : variable"
+    p[0] = Node.VarExp(var=p[1])
+
+def p_nil_exp(p):
+    "nil_exp : NIL"
+    p[0] = Node.NilExp(position=Position(lineNumber=-1)) #TODO: Change placeholder.
+
+def p_int_exp(p):
+    "int_exp : INT"
+    p[0] = Node.IntExp(int=p[1])
+
+def p_string_exp(p):
+    "string_exp : STRING"
+    p[0] = Node.StringExp(string=p[1])
+
+def p_call_exp(p):
+    "call_exp : ID LPAREN arg_list RPAREN"
+    p[0] = Node.CallExp(func=p[1], args=p[3])
+
+def p_arg_list(p):
+    """
+    arg_list : empty_list
+             | exp_list
+    """
+    p[0] = p[1]
+
+# Non-empty expression list.
+def p_exp_list(p):
+    """
+    exp_list : exp_list_iter
+             | exp_list_end
+    """
+    p[0] = p[1]
+
+
+def p_exp_list_iter(p):
+    "exp_list_iter : exp_list COMMA expression"
+    p[0] = p[1]
+    p[0].append(p[2])
+
+
+def p_exp_list_end(p):
+    "exp_list_end : expression"
+    p[0] = [p[1]]
+
+def p_op_exp(p):
+    """
+    op_exp : unary_minus_exp
+           | binary_plus_exp
+           | binary_minus_exp
+           | binary_times_exp
+           | binary_divide_exp
+           | binary_eq_exp
+           | binary_neq_exp
+           | binary_lt_exp
+           | binary_le_exp
+           | binary_gt_exp
+           | binary_ge_exp
+           | binary_and_exp
+           | binary_or_exp
+    """
+    p[0] = p[1]
+
+def p_unary_minus_exp(p):
+    "unary_minus_exp : MINUS expression"
+    p[0] = Node.OpExp(oper=minus, left=0, right=p[2])
+
+def p_binary_plus_exp(p):
+    "binary_plus_exp : expression PLUS expression"
+    p[0] = Node.OpExp(oper=plus, left=p[1], right=p[3])
+
+def p_binary_minus_exp(p):
+    "binary_minus_exp : expression MINUS expression"
+    p[0] = Node.OpExp(oper=minus, left=p[1], right=p[3])
+
+def p_binary_times_exp(p):
+    "binary_times_exp : expression TIMES expression"
+    p[0] = Node.OpExp(oper=times, left=p[1], right=p[3])
+
+def p_binary_divide_exp(p):
+    "binary_divide_exp : expression DIVIDE expression"
+    p[0] = Node.OpExp(oper=divide, left=p[1], right=p[3])
+
+def p_binary_eq_exp(p):
+    "binary_eq_exp : expression EQ expression"
+    p[0] = Node.OpExp(oper=eq, left=p[1], right=p[3])
+
+def p_binary_neq_exp(p):
+    "binary_neq_exp : expression NEQ expression"
+    p[0] = Node.OpExp(oper=neq, left=p[1], right=p[3])
+
+def p_binary_lt_exp(p):
+    "binary_lt_exp : expression LT expression"
+    p[0] = Node.OpExp(oper=lt, left=p[1], right=p[3])
+
+def p_binary_le_exp(p):
+    "binary_le_exp : expression LE expression"
+    p[0] = Node.OpExp(oper=le, left=p[1], right=p[3])
+
+def p_binary_gt_exp(p):
+    "binary_gt_exp : expression GT expression"
+    p[0] = Node.OpExp(oper=gt, left=p[1], right=p[3])
+
+def p_binary_ge_exp(p):
+    "binary_ge_exp : expression GE expression"
+    p[0] = Node.OpExp(oper=ge, left=p[1], right=p[3])
+
+def p_binary_and_exp(p):
+    "binary_and_exp : expression AND expression"
+    p[0] = Node.IfExp(test=p[1], then=p[3], elsee=IntExp(int=0))
+
+def p_binary_or_exp(p):
+    "binary_or_exp : expression OR expression"
+    p[0] = Node.IfExp(test=p[1], then=IntExp(int=1), elsee=p[3])
+
+def p_record_exp(p):
+    "record_exp : ID LBRACE exp_field_list RBRACE"
+    p[0] = Node.RecordExp(type=p[1], fields=p[3])
+
+def p_exp_field(p):
+    "exp_field : ID EQ expression"
+    p[0] = Node.ExpField(name=p[1], exp=p[3])
+
+def p_exp_field_list(p):
+    """
+    exp_field_list : empty_list
+                   | ne_exp_field_list
+    """
+    p[0] = p[1]
+
+# Non-empty expression list.
+def p_ne_exp_field_list(p):
+    """
+    ne_exp_field_list : ne_exp_field_list_iter
+                      | ne_exp_field_list_end
+    """
+    p[0] = p[1]
+
+
+def p_ne_exp_field_list_iter(p):
+    "ne_exp_field_list_iter : ne_exp_field_list COMMA exp_field"
+    p[0] = p[1]
+    p[0].append(p[2])
+
+
+def p_ne_exp_field_list_end(p):
+    "ne_exp_field_list_end : exp_field"
+    p[0] = [p[1]]
+
+#TODO: This should be two or more.
+def p_seq_exp(p):
+    "seq_exp : LPAREN ne_exp_seq RPAREN"
+    p[0] = Node.SeqExp(seq=p[1])
+
+# Non-empty expression sequence.
+def p_ne_exp_seq(p):
+    """
+    ne_exp_seq : ne_exp_seq_iter
+               | ne_exp_seq_end
+    """
+    p[0] = p[1]
+
+
+def p_ne_exp_seq_iter(p):
+    "ne_exp_seq_iter : ne_exp_seq SEMICOLON expression"
+    p[0] = p[1]
+    p[0].append(p[2])
+
+
+def p_ne_exp_seq_end(p):
+    "ne_exp_seq_end : expression"
+    p[0] = [p[1]]
+
+
+def p_assign_exp(p):
+    "assign_exp : variable ASSIGN expression"
+    p[0] = Node.VarExp(var=p[1], exp=p[3])
+
+def p_if_exp(p):
+    "if_exp : IF expression THEN expression ELSE expression"
+    p[0] = Node.IfExp(test=p[2], then=p[4], elsee=p[6])
+
+def p_while_exp(p):
+    "while_exp : WHILE expression DO expression"
+    p[0] = Node.WhileExp(test=p[2], body=p[4])
+
+def p_break_exp(p):
+    "break_exp : BREAK"
+    p[0] = Node.BreakExp(position=-1) #TODO: Change placeholder.
+
+def p_for_exp(p):
+    "for_exp : FOR variable ASSIGN expression TO expression DO expression"
+    p[0] = Node.VarExp(var=p[2], lo=p[4], hi=p[6], body=p[8])
+
+def p_let_exp(p):
+    "let_exp : LET declaration_block IN exp_seq END"
+    p[0] = Node.VarExp(decs=p[2], body=p[4])
+
+def p_exp_seq(p):
+    """
+    exp_seq : empty_list
+            | ne_exp_seq
+    """
+    p[0] = p[1]
+
+# TODO: Add to expression rule.
+# def p_array_exp(p):
+#     "array_exp : variable"
+#     p[0] = Node.VarExp(var=p[1])
+
+# VARIABLE
+
+def p_variable(p):
+    """
+    variable : simple_var
+             | field_var
+             | subscript_var
+    """
+    p[0] = p[1]
+
+def p_simple_var(p):
+    "simple_var : ID"
+    p[0] = Node.SimpleVar(sym=p[1])
+
+def p_field_var(p):
+    "field_var : variable DOT ID"
+    p[0] = Node.FieldVar(var=p[1], sym=p[3])
+
+def p_subscript_var(p):
+    "subscript_var : variable LBRACK expression RBRACK"
+    p[0] = Node.SubscriptVar(var=p[1], exp=p[3])
+
+# ERROR
 def p_error(p):
     print("Syntax error in input!")
-
 
 # Build the parser
 parser = yacc.yacc()
