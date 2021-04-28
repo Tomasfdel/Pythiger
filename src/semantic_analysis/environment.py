852ecf64db163058ea899666515d4e9c374bdc3d
Tomasfdel <tomasfdel@gmail.com>
2021-04-28 19:47:05 -0300

    Defined the type system and the base environments.

---
+++ src/semantic_analysis/environment.py
@@ -0,0 +1,46 @@
+from typing import List
+
+from dataclasses import dataclass
+
+from src.semantic_analysis.table import SymbolTable
+from src.semantic_analysis.types import Type, IntType, StringType, VoidType
+
+
+class EnvironmentEntry:
+    pass
+
+
+@dataclass
+class VariableEntry(EnvironmentEntry):
+    type: Type
+    is_editable: bool = True
+
+
+@dataclass
+class FunctionEntry(EnvironmentEntry):
+    formals: List[Type]
+    result: Type
+
+
+def base_type_environment() -> SymbolTable[Type]:
+    environment = SymbolTable[Type]()
+    environment.add("int", IntType())
+    environment.add("string", StringType())
+    return environment
+
+
+def base_value_environment() -> SymbolTable[EnvironmentEntry]:
+    environment = SymbolTable[EnvironmentEntry]()
+    environment.add("print", FunctionEntry([StringType()], VoidType()))
+    environment.add("flush", FunctionEntry([], VoidType()))
+    environment.add("getchar", FunctionEntry([], StringType()))
+    environment.add("ord", FunctionEntry([StringType()], IntType()))
+    environment.add("chr", FunctionEntry([IntType()], StringType()))
+    environment.add("size", FunctionEntry([StringType()], IntType()))
+    environment.add(
+        "substring", FunctionEntry([StringType(), IntType(), IntType()], StringType())
+    )
+    environment.add("concat", FunctionEntry([StringType(), StringType()], StringType()))
+    environment.add("not", FunctionEntry([IntType()], IntType()))
+    environment.add("exit", FunctionEntry([IntType()], VoidType()))
+    return environment
---
+++ src/semantic_analysis/types.py
@@ -0,0 +1,58 @@
+from typing import List
+
+from dataclasses import dataclass
+
+
+class Type:
+    pass
+
+
+class NilType(Type):
+    pass
+
+
+class IntType(Type):
+    pass
+
+
+class StringType(Type):
+    pass
+
+
+class VoidType(Type):
+    pass
+
+
+@dataclass
+class Field:
+    name: str
+    type: Type
+
+
+@dataclass
+class RecordType(Type):
+    fields: List[Field]
+
+
+@dataclass
+class ArrayType(Type):
+    type: Type
+
+
+@dataclass
+class NameType(Type):
+    symbol: str
+
+
+def are_types_equal(t1: Type, t2: Type) -> bool:
+    return (
+        isinstance(t1, NilType)
+        and isinstance(t2, NilType)
+        or isinstance(t1, IntType)
+        and isinstance(t2, IntType)
+        or isinstance(t1, StringType)
+        and isinstance(t2, StringType)
+        or isinstance(t1, VoidType)
+        and isinstance(t2, VoidType)
+        or t1 is t2
+    )
