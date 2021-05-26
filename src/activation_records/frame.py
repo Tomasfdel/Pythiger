b4d51fabe011049139a5582390a0009309ae4a81
Tomasfdel <tomasfdel@gmail.com>
2021-05-26 16:32:35 -0300

    Some changes so flake8 passes.

--- src/activation_records/frame.py
+++ src/activation_records/frame.py
@@ -1,43 +1,7 @@
+from dataclasses import dataclass
+
 from temp import Temp, TempLabel, TempManager
 from abc import ABC
-
- # it's a data structure holding:
- # the locations of all the formals
- # instructions required to implement the "view shift"
- # the number of locals allocated so far
- # the label at which the function's machine code is to begin.
-class Frame:
-  #Creates a new frame for function "name" with "formalEscapes" list of
-  #booleans(list of parameters for function "name"). True means
-  #escaped variable.
-  __init__(self, name: TempLabel, formalEscapes: [bool])
-    self.name = name
-    # The previous %rbp value is stored at %rbp.
-    # Non-volatile registers are stored starting at -8(%rbp).
-    self.offset = -8
-    # List of "accesses" denoting the locations where
-    # the formal parameters will be kept at run time, as seen from
-    # inside the callee.
-    self.formalParameters = []
-    for escape in formalEscapes:
-        allocSingleVar(escape)
-
-  # Allocates a new local variable in the frame. The "escape"
-  # variable indicates whether the variable escapes or not
-  def allocLocal(self, escape: bool) -> InFrame:
-      return allocSingleVar(escape)
-
-  def allocSingleVar(self, escape: bool) -> InFrame
-    if escape:
-        self.offset -= 8
-        self.formalParameters.append(InFrame(self.offset))
-    else:
-        self.formalParameters.append(InRegister(TempManager.new_temp))
-    return self.formalParameters[-1]
-
-  # Instruciones requeridas para implementar "cambio de vista"
-
-  # El numero de locales alocados hasta el momento
 
 
 # La clase Access describe formals y locals que están en frame
@@ -46,15 +10,56 @@ class Frame:
 # Fijate si podes poner Access solo total el modulo te maskea los
 # nombres.
 class FrameAccess(ABC):
-  pass
+    pass
+
 
 # InFrame(X) indica una ubicación de memoria en el offset X respecto
 # al frame pointer, es decir, %rbp.
 @dataclass
 class InFrame(FrameAccess):
-  offset: int
+    offset: int
+
 
 # InReg(t84) indica que se va a mantener en el "registro" t84.
 @dataclass
 class InRegister(FrameAccess):
-  register: Temp
+    register: Temp
+
+
+# it's a data structure holding:
+# the locations of all the formals
+# instructions required to implement the "view shift"
+# the number of locals allocated so far
+# the label at which the function's machine code is to begin.
+class Frame:
+    # Creates a new frame for function "name" with "formalEscapes" list of
+    # booleans(list of parameters for function "name"). True means
+    # escaped variable.
+    def __init__(self, name: TempLabel, formal_escapes: [bool]):
+        self.name = name
+        # The previous %rbp value is stored at %rbp.
+        # Non-volatile registers are stored starting at -8(%rbp).
+        self.offset = -8
+        # List of "accesses" denoting the locations where
+        # the formal parameters will be kept at run time, as seen from
+        # inside the callee.
+        self.formalParameters = []
+        for escape in formal_escapes:
+            self.alloc_single_var(escape)
+
+    # Allocates a new local variable in the frame. The "escape"
+    # variable indicates whether the variable escapes or not
+    def alloc_local(self, escape: bool) -> InFrame:
+        return self.alloc_single_var(escape)
+
+    def alloc_single_var(self, escape: bool) -> InFrame:
+        if escape:
+            self.offset -= 8
+            self.formalParameters.append(InFrame(self.offset))
+        else:
+            self.formalParameters.append(InRegister(TempManager.new_temp()))
+        return self.formalParameters[-1]
+
+    # Instruciones requeridas para implementar "cambio de vista"
+
+    # El numero de locales alocados hasta el momento
--- src/activation_records/temp.py
+++ src/activation_records/temp.py
@@ -3,22 +3,24 @@ from abc import ABC
 Temp = int
 TempLabel = str
 
+
 class TempManager(ABC):
-  tempCount = 0
-  labelCount = 0
-  @classmethod
-  def new_temp(cls) -> Temp:
-    tempCount += 1
-    return tempCount
+    tempCount = 0
+    labelCount = 0
 
-  @classmethod
-  def new_label(cls) -> TempLabel:
-    labelCount += 1
-    return f"lab_{labelCount}"
+    @classmethod
+    def new_temp(cls) -> Temp:
+        cls.tempCount += 1
+        return cls.tempCount
 
-  # TODO: Esto es necesario?
-  # Si es necesario, que pasa cuando ya existe la label?
-  # Parece que nada, nadie puso guards.
-  @classmethod
-  def named_label(cls, name: str) -> TempLabel:
-    return name
+    @classmethod
+    def new_label(cls) -> TempLabel:
+        cls.labelCount += 1
+        return f"lab_{cls.labelCount}"
+
+    # TODO: Esto es necesario?
+    # Si es necesario, que pasa cuando ya existe la label?
+    # Parece que nada, nadie puso guards.
+    @classmethod
+    def named_label(cls, name: str) -> TempLabel:
+        return name
