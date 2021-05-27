ec60b9f5076386f1b91c0e36ce48d5c742edaf4c
Tomasfdel <tomasfdel@gmail.com>
2021-05-27 18:55:21 -0300

    Added frame.py definitions found in Chapter 7.

--- src/activation_records/frame.py
+++ src/activation_records/frame.py
@@ -1,5 +1,9 @@
+from typing import List
+
 from dataclasses import dataclass
 
+from intermediate_representation.translated_expression import Expression
+from intermediate_representation.tree import Statement
 from temp import Temp, TempLabel, TempManager
 from abc import ABC
 
@@ -63,3 +67,59 @@ class Frame:
     # Instruciones requeridas para implementar "cambio de vista"
 
     # El numero de locales alocados hasta el momento
+
+
+# Extra definitions found on Chapter 7. Feel free to rename them as you consider.
+
+# Frame pointer register FP.
+# F_FP: Temp
+
+# Constant for the machine's word size.
+# F_wordSize: int
+
+
+# This function is used by Translate to turn a Frame_access into an intermediate representation
+# Tree expression. The Tree_exp argument is the address of the stack frame that the access lives in.
+# If acc is a register access, such as InReg(t82), then the frame address argument will be discarded
+# and the result will be simply Temp(t82).
+def frame_exp(access: FrameAccess, frame_ptr: Expression) -> Expression:
+    pass
+
+
+# Sometimes we will need to call external functions that as written in C or assembly language
+# (such as a function that allocates memory for a Tiger array).
+# However, sometimes the C compiler puts an underscore at the beginning of each label, or the
+# calling convention for C functions may differ from those of Tiger functions. All these target
+# machine specific details should be encapsulated in this function, which creates a Tree expression
+# that creates the relevant function call. The simplest form for this function would be something
+# like return T_Call(T_Name(Temp_namedlabel(s)), args); ,
+# but it may have to be adapted for static links, underscores in labels, and so on.
+def external_call(function_name: str, arguments: List[Expression]) -> Expression:
+    pass
+
+
+# Location of a function's return value (RV) as specified by the machine-specific frame structure.
+# F_RV: Temp
+
+
+# This applies the view shift of calling a function, which is explained in Chapter 6.
+# Looks like this method is explained later, so we can use a dummy implementation
+# that just returns stm for now.
+def F_procEntryExit1(frame: Frame, statement: Statement) -> Statement:
+    return statement
+
+
+class Fragment(ABC):
+    pass
+
+
+@dataclass
+class StringFragment(Fragment):
+    label: TempLabel
+    string: str
+
+
+@dataclass
+class ProcessFragment(Fragment):
+    body: Statement
+    frame: Frame
