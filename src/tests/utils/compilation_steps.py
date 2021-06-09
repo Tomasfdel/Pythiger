616a4d35dbad3e0c61db89cd420f1e30992c4bfd
Tomasfdel <tomasfdel@gmail.com>
2021-06-09 00:01:31 -0300

    Extracted compilation steps into a test auxiliary file.

---
+++ src/tests/utils/compilation_steps.py
@@ -0,0 +1,18 @@
+from lexer import lex as le
+from parser import parser as p
+from ply import lex
+
+import parser.ast_nodes as ast
+from semantic_analysis.analyzers import TypedExpression, translate_program
+
+
+def parse_program(file_name: str) -> ast.Expression:
+    with open("examples/" + file_name, "r") as file:
+        data = file.read()
+    le.lexer.lineno = 1
+    lex.input(data)
+    return p.parser.parse(data, le.lexer)
+
+
+def semantic_analysis(file_name: str) -> TypedExpression:
+    return translate_program(parse_program(file_name))
