5381454fbe30997518c9bda0e57c5a208c526a80
Tomasfdel <tomasfdel@gmail.com>
2021-07-16 18:00:53 -0300

    Removed unused methods and cleared TODOs of the Graph implementation.

--- src/liveness_analysis/graph.py
+++ src/liveness_analysis/graph.py
@@ -11,8 +11,6 @@ class Node(Generic[T]):
     information: T
 
 
-# TODO: Many of these methods are not used in liveness analysis. Consider deleting them.
-# TODO: Consider defining a add_undirected_edge and remove_undirected_edge.
 class Graph(Generic[T]):
     def __init__(self):
         self.nodes = []
@@ -33,25 +31,8 @@ class Graph(Generic[T]):
         self.out_edges[origin.id].add(destination.id)
         self.in_edges[destination.id].add(origin.id)
 
-    def remove_edge(self, origin: Node[T], destination: Node[T]):
-        self.out_edges[origin.id].remove(destination.id)
-        self.in_edges[destination.id].remove(origin.id)
-
     def node_successors(self, node: Node[T]) -> List[Node[T]]:
         return [self.nodes[successor_id] for successor_id in self.out_edges[node.id]]
 
     def node_predecessors(self, node: Node[T]) -> List[Node[T]]:
         return [self.nodes[predecessor_id] for predecessor_id in self.in_edges[node.id]]
-
-    def node_adjacent(self, node: Node[T]) -> List[Node[T]]:
-        adjacent_ids = self.in_edges[node.id].union(self.out_edges[node.id])
-        return [self.nodes[id] for id in adjacent_ids]
-
-    def node_goes_to(self, origin: Node[T], destination: Node[T]) -> bool:
-        return destination.id in self.out_edges[origin.id]
-
-    def are_nodes_adjacent(self, node1: Node[T], node2: Node[T]) -> bool:
-        return self.node_goes_to(node1, node2) or self.node_goes_to(node2, node1)
-
-    def node_degree(self, node: Node[T]) -> int:
-        return len(self.node_predecessors(node)) + len(self.node_successors(node))
--- src/liveness_analysis/tests/test_graph.py
+++ src/liveness_analysis/tests/test_graph.py
@@ -3,7 +3,7 @@ import unittest
 from liveness_analysis.graph import Graph
 
 
-class TestSymbolTable(unittest.TestCase):
+class TestGraph(unittest.TestCase):
     def setUp(self):
         self.graph = Graph[int]()
 
@@ -44,83 +44,3 @@ class TestSymbolTable(unittest.TestCase):
 
         self.assertIn(origin, self.graph.node_predecessors(destination))
         self.assertNotIn(unrelated, self.graph.node_predecessors(destination))
-
-    def test_add_edge_idempotence(self):
-        origin = self.graph.add_node(0)
-        destination = self.graph.add_node(1)
-
-        self.assertEqual(self.graph.node_successors(origin), [])
-
-        self.graph.add_edge(origin, destination)
-        self.assertIn(destination, self.graph.node_successors(origin))
-
-        self.graph.add_edge(origin, destination)
-        self.assertIn(destination, self.graph.node_successors(origin))
-
-        self.graph.remove_edge(origin, destination)
-        self.assertEqual(self.graph.node_successors(origin), [])
-
-    def test_node_adjacent(self):
-        origin = self.graph.add_node(0)
-        middle = self.graph.add_node(1)
-        destination = self.graph.add_node(2)
-
-        self.assertEqual(self.graph.node_adjacent(middle), [])
-
-        self.graph.add_edge(origin, middle)
-        self.assertIn(origin, self.graph.node_adjacent(middle))
-        self.assertIn(middle, self.graph.node_adjacent(origin))
-
-        self.graph.add_edge(middle, destination)
-        self.assertIn(middle, self.graph.node_adjacent(destination))
-        self.assertIn(destination, self.graph.node_adjacent(middle))
-
-    def test_node_adjacent_back_and_forth_edges(self):
-        node1 = self.graph.add_node(0)
-        node2 = self.graph.add_node(1)
-
-        self.graph.add_edge(node1, node2)
-        self.graph.add_edge(node2, node1)
-
-        self.assertEqual(len(self.graph.node_adjacent(node1)), 1)
-        self.assertEqual(len(self.graph.node_adjacent(node2)), 1)
-
-    def test_node_goes_to(self):
-        origin = self.graph.add_node(0)
-        destination = self.graph.add_node(1)
-
-        self.assertFalse(self.graph.node_goes_to(origin, destination))
-        self.assertFalse(self.graph.node_goes_to(destination, origin))
-
-        self.graph.add_edge(origin, destination)
-        self.assertTrue(self.graph.node_goes_to(origin, destination))
-        self.assertFalse(self.graph.node_goes_to(destination, origin))
-
-        self.graph.remove_edge(origin, destination)
-        self.assertFalse(self.graph.node_goes_to(origin, destination))
-        self.assertFalse(self.graph.node_goes_to(destination, origin))
-
-    def test_node_degree_single_edge(self):
-        origin = self.graph.add_node(0)
-        destination = self.graph.add_node(1)
-
-        self.assertEqual(self.graph.node_degree(origin), 0)
-        self.assertEqual(self.graph.node_degree(destination), 0)
-
-        self.graph.add_edge(origin, destination)
-        self.assertEqual(self.graph.node_degree(origin), 1)
-        self.assertEqual(self.graph.node_degree(destination), 1)
-
-        self.graph.remove_edge(origin, destination)
-        self.assertEqual(self.graph.node_degree(origin), 0)
-        self.assertEqual(self.graph.node_degree(destination), 0)
-
-    def test_node_degree_back_and_forth_edge(self):
-        node1 = self.graph.add_node(0)
-        node2 = self.graph.add_node(1)
-
-        self.graph.add_edge(node1, node2)
-        self.graph.add_edge(node2, node1)
-
-        self.assertEqual(self.graph.node_degree(node1), 2)
-        self.assertEqual(self.graph.node_degree(node2), 2)
