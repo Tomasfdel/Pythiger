e63893ec12e55fe6fbb861b48e72d1341ea2f63f
Weheineman <gi4nn1@yahoo.de>
2021-08-01 20:31:34 -0300

    TODO cleanup. * Made temp_to_str throw an exception when it fails.

--- src/activation_records/frame.py
+++ src/activation_records/frame.py
@@ -70,13 +70,10 @@ def return_value() -> Temp:
     return TempMap.register_to_temp["rax"]
 
 
-# TODO: Placeholder for testing.
+# This is called after register allocation. All temps have an assigned
+# register in the TempMap (if not, this should fail loudly).
 def temp_to_str(temp: Temp) -> str:
-    register = TempMap.temp_to_register.get(temp)
-    if register:
-        return register
-
-    return f"t{temp}"
+    return TempMap.temp_to_register[temp]
 
 
 # Access describes formals and locals stored in a frame or in registers
@@ -160,13 +157,6 @@ def access_to_exp(access: Access, frame_pointer: IRT.Expression) -> IRT.Expressi
 
 # Sometimes we will need to call external functions that as written in C or assembly language
 # (such as a function that allocates memory for a Tiger array).
-# However, sometimes the C compiler puts an underscore at the beginning of each label, or
-# the calling convention for C functions may differ from those of Tiger functions. All these target
-# machine specific details should be encapsulated in this function, which creates a Tree expression
-# that creates the relevant function call. The simplest form for this function would be something
-# like return T_Call(T_Name(Temp_namedlabel(s)), args); , but it may have to be adapted for
-# static links, underscores in labels, and so on.
-# TODO: See what shape this expression should have once we move to define the runtime functions.
 def external_call(
     function_name: str, arguments: List[IRT.Expression]
 ) -> IRT.Expression:
@@ -232,7 +222,6 @@ def preserve_callee_registers(
 # This function appends a “sink” instruction to the function body to tell the
 # register allocator that certain registers are live at procedure exit.
 def sink(function_body: List[Assembly.Instruction]) -> List[Assembly.Instruction]:
-    # TODO: Check if rax needs to be in this list.
     sink_registers = callee_saved_registers + ["rsp", "rip"]
     sink_temps = [TempMap.register_to_temp[register] for register in sink_registers]
     function_body.append(
@@ -293,5 +282,4 @@ def assembly_procedure(
 
 
 def string_literal(label: TempLabel, string: str) -> str:
-    # TODO: Do we use .asciz or .string?
     return f"{label}:\n\t.asciz {string}\n"
